d6347f6daef10655602b19aba6623984d2a42e1b
Julien Salort <mail@juliensalort.org>
2016-03-14 10:47:00 +0000

    Power supply tti_tsx3510p added in quick links

--- pymanip/instruments/__init__.py
+++ pymanip/instruments/__init__.py
@@ -7,3 +7,4 @@ from fluidlab.instruments.multiplexer.agilent_34970a import Agilent34970a
 # Power supply
 from fluidlab.instruments.powersupply.isotech_ips2303s import IsoTechIPS2303S
 from fluidlab.instruments.powersupply.tdk_lambda import TdkLambda
+from fluidlab.instruments.funcgen.tti_tsx3510p import TtiTsx3510p
