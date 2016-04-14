f599e73fe713a683bf2c9b8a35928750f5712151
Julien <julien.salort@ens-lyon.fr>
2016-04-14 18:22:33 +0200

    Add Agilent 6030a

--- pymanip/instruments/__init__.py
+++ pymanip/instruments/__init__.py
@@ -13,4 +13,7 @@ from fluidlab.instruments.multimeter.hp_34401a import HP34401a
 # Power supply
 from fluidlab.instruments.powersupply.isotech_ips2303s import IsoTechIPS2303S
 from fluidlab.instruments.powersupply.tdk_lambda import TdkLambda
+from fluidlab.instruments.powersupply.agilent_6030a import Agilent6030a
+
+# Function generators
 from fluidlab.instruments.funcgen.tti_tsx3510p import TtiTsx3510p
