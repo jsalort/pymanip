f3ea01bc1295deb4a8d47647fc231d6ca27b512f
Julien Salort <mail@juliensalort.org>
2017-01-25 18:55:34 +0100

    PyPi 0.2 version

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from setuptools import setup
+from distutils.core import setup
 
 def readme():
     with open('README.md') as f:
         return f.read()
 
 setup(name='pymanip',
-      version='0.1',
+      version='0.2',
       description='Python framework for experiments',
       long_description=readme(),
       url='https://github.com/jsalort/pymanip',
@@ -16,8 +16,6 @@ setup(name='pymanip',
       author_email='julien.salort@ens-lyon.fr',
       license='CeCILL-B',
       packages=['pymanip'],
-      install_requires=[
-        'h5py', #'clint', 'fluidlab', 'fluiddyn',
-      ],
-      include_package_data=True,
-      zip_safe=False)
+      requires=[
+        'h5py', 'clint', 'fluidlab (>=0.0.3)', 'fluiddyn',
+      ])
