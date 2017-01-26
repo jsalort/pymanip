e0d0b4c57bdbb3832a93fb699fdfe9adde6a36cd
Julien Salort <mail@juliensalort.org>
2017-01-26 11:13:26 +0100

    Revert to setuptools to allow develop option

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from distutils.core import setup
+#from distutils.core import setup
+from setuptools import setup
 
 def readme():
     with open('README.md') as f:
@@ -16,6 +17,6 @@ setup(name='pymanip',
       author_email='julien.salort@ens-lyon.fr',
       license='CeCILL-B',
       packages=['pymanip'],
-      requires=[
+      install_requires=[
         'h5py', 'clint', 'fluidlab (>=0.0.3)', 'fluiddyn',
       ])
