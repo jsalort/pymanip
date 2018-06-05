7890505644734f1134a387fa53445398a26ee410
Julien Salort <mail@juliensalort.org>
2018-06-06 00:30:00 +0200

    Add dependencies in setup

--- setup.py
+++ setup.py
@@ -4,9 +4,11 @@
 #from distutils.core import setup
 from setuptools import setup
 
+
 def readme():
     with open('README.md') as f:
         return f.read()
+
 
 setup(name='pymanip',
       version='0.3b0',
@@ -17,6 +19,6 @@ setup(name='pymanip',
       author_email='julien.salort@ens-lyon.fr',
       license='CeCILL-B',
       packages=['pymanip'],
-      install_requires=[
-        'h5py', 'clint', 'fluidlab (>=0.0.3)', 'fluiddyn',
-      ])
+      install_requires=['h5py', 'clint', 'fluidlab (>=0.0.3)',
+                        'opencv-python', 'progressbar2',
+                        'aiohttp', 'aiohttp_jinja2'])
