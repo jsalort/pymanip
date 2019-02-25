8b41cb41ccba38f855469272c0716393766dd358
Julien Salort <julien@salort.eu>
2019-02-25 18:19:29 +0100

    Move fluidlab imports into separate files

---
+++ pymanip/instruments/fluidlab_instruments.py
@@ -0,0 +1,56 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+
+This module contains all the instrument classes available in fluidlab.
+
+List of supported instruments:
+
+"""
+
+from __future__ import print_function
+import sys
+import fluidlab.instruments as instruments
+import pymanip
+
+if hasattr(pymanip, "pymanip_import_verbose"):
+    verbose_import = pymanip.pymanip_import_verbose
+else:
+    verbose_import = False
+    
+__all__ = []
+
+if verbose_import:
+    print('Supported instruments:')
+
+for instrument_type in instruments.__all__:
+    exec("import fluidlab.instruments." + instrument_type + " as " + instrument_type)
+    exec("instrument_type_module = " + instrument_type)
+    __doc__ += '* ' + instrument_type + ': '
+    if verbose_import:
+        sys.stdout.write('* ' + instrument_type + ': ')
+    for instrument_file in instrument_type_module.__all__:
+        try:
+            exec("import fluidlab.instruments." + instrument_type + "." + instrument_file + " as " + instrument_file)
+            exec("instrument_file_module = " + instrument_file)
+            for instrument_classname in instrument_file_module.__all__:
+                exec("from fluidlab.instruments." + instrument_type + "." + instrument_file + " import " + instrument_classname)
+                __doc__ += instrument_classname + " "
+                if verbose_import:
+                    sys.stdout.write(instrument_classname + " ")
+                __all__.append(instrument_classname)
+        except ImportError:
+            print('Unable to import', instrument_type + '.' + instrument_file + '.' + instrument_classname)
+            
+    __doc__ += "\n"
+    if verbose_import:
+        sys.stdout.write("\n")
+
+__doc__ += """
+Example of usage:
+
+from pymanip.instruments import Agilent34401a
+from pymanip.interfaces import GPIBInterface
+
+multiplexer = Agilent34401a(GPIBInterface(0,5))
+"""
---
+++ pymanip/interfaces/fluidlab_interfaces.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+import platform
+
+if platform.system() == 'Linux':
+    try:
+        from fluidlab.instruments.interfaces.linuxgpib import GPIBInterface
+    except ImportError:
+        print('Warning: Linux-GPIB Python bindings not found')
+        pass
+    try:
+        from fluidlab.instruments.interfaces.linuxusbtmc import LinuxUSBTMCInterface
+    except ImportError:
+        print('Warning: Linux USBTMC bindings not found.')
+        pass
+
+try:
+    from fluidlab.instruments.interfaces.visa import PyvisaInterface
+except ImportError:
+    print('Warning: PyVISA not found')
+    pass
+
+try:
+    from fluidlab.instruments.interfaces.serial_inter import SerialInterface
+except ImportError:
+    print('Warning: SerialInterface not found')
+    pass
