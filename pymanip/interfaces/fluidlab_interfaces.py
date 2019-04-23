bbfdaa372af66cbf8b306bc500689da7755f9117
Julien Salort <mail@juliensalort.org>
2019-04-23 15:42:56 +0200

    Update to new fluidlab interface hierarchy

--- pymanip/interfaces/fluidlab_interfaces.py
+++ pymanip/interfaces/fluidlab_interfaces.py
@@ -1,29 +1,31 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
-
-from __future__ import print_function
 import platform
+
+__all__ = list()
 
 if platform.system() == 'Linux':
     try:
-        from fluidlab.instruments.interfaces.linuxgpib import GPIBInterface
+        from fluidlab.interfaces.gpib_inter import GPIBInterface
+        __all__.append('GPIBInterface')
     except ImportError:
         print('Warning: Linux-GPIB Python bindings not found')
         pass
     try:
-        from fluidlab.instruments.interfaces.linuxusbtmc import LinuxUSBTMCInterface
+        from fluidlab.interfaces.usbtmc_inter import USBTMCInterface
+        __all__.append('USBTMCInterface')
     except ImportError:
         print('Warning: Linux USBTMC bindings not found.')
         pass
 
 try:
-    from fluidlab.instruments.interfaces.visa import PyvisaInterface
+    from fluidlab.interfaces.visa_inter import VISAInterface
+    __all__.append('VISAInterface')
 except ImportError:
     print('Warning: PyVISA not found')
     pass
 
 try:
-    from fluidlab.instruments.interfaces.serial_inter import SerialInterface
+    from fluidlab.interfaces.serial_inter import SerialInterface
+    __all__.append('SerialInterface')
 except ImportError:
     print('Warning: SerialInterface not found')
     pass
