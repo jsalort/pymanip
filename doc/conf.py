75439f9d88572e29a1baf7a8ebdd3804819cc270
Julien Salort <mail@juliensalort.org>
2019-11-28 19:59:35 +0100

    Add intersphinx link to fluidlab and use sphinx_rtd theme

--- doc/conf.py
+++ doc/conf.py
@@ -30,7 +30,8 @@ release = "0.3"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autodoc", "sphinxarg.ext"]
+extensions = ["sphinx.ext.autodoc", "sphinxarg.ext", "sphinx.ext.intersphinx"]
+intersphinx_mapping = {"fluidlab": ("https://fluidlab.readthedocs.io/en/latest/", None)}
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
@@ -48,7 +49,8 @@ master_doc = "index"
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "alabaster"
+# html_theme = "alabaster"
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
