ea40f915aeb54bc1777f907faf08855f75cc7314
Julien Salort <julien@salort.eu>
2019-11-28 23:33:57 +0100

    Update fluidlab version requirement

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     install_requires=[
         "h5py",
         "clint",
-        "fluidlab (>=0.0.3)",
+        "fluidlab (>=0.1.0)",
         "progressbar2",
         "aiohttp",
         "aiohttp_jinja2",
