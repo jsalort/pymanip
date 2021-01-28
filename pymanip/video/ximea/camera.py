025dc036a6507d49629bfc16a58193967f616c1d
Julien Salort <julien@salort.eu>
2021-01-28 21:51:55 +0100

    More informative closing message

--- pymanip/video/ximea/camera.py
+++ pymanip/video/ximea/camera.py
@@ -20,11 +20,13 @@ from pymanip.video import MetadataArray, Camera, CameraTimeout
 from ximea import xiapi
 from ximea.xidefs import XI_IMG_FORMAT, XI_TRG_SOURCE, XI_TRG_SELECTOR
 
+
 class Ximea_Camera(Camera):
     """Concrete :class:`pymanip.video.Camera` class for AVT camera.
     """
-    color_order = "BGR" # Ximea RGB mode is [blue][green][red] per the doc.
-    
+
+    color_order = "BGR"  # Ximea RGB mode is [blue][green][red] per the doc.
+
     def __init__(self, serial_number=None, pixelFormat=None):
         # pour le moment on ouvre simplement la première caméra
         self.cam = xiapi.Camera()
@@ -38,26 +40,29 @@ class Ximea_Camera(Camera):
             else:
                 pixelFormat = "XI_MONO8"
         elif pixelFormat not in XI_IMG_FORMAT:
-            raise ValueError(f"Wrong pixelFormat. Possible values are {set(XI_IMG_FORMAT.keys()):}")
+            raise ValueError(
+                f"Wrong pixelFormat. Possible values are {set(XI_IMG_FORMAT.keys()):}"
+            )
         self.cam.set_imgdataformat(pixelFormat)
         print(f"Camera {self.name:} opened ({pixelFormat:})")
-    
+
     def close(self):
+        name = self.name
         self.cam.close_device()
         self.cam = None
-        print("Connection to camera closed")
-    
+        print(f"Connection to camera {name:} closed.")
+
     def __exit__(self, type_, value, cb):
         """Context manager exit method
         """
         super().__exit__(type_, value, cb)
         self.close()
-    
+
     @property
     def name(self):
         """Camera name
         """
-        return self.cam.get_device_name().decode('ascii')
+        return self.cam.get_device_name().decode("ascii")
 
     def set_exposure_time(self, seconds):
         """This method sets the exposure time for the camera.
@@ -65,22 +70,22 @@ class Ximea_Camera(Camera):
         :param seconds: exposure in seconds.
         :type seconds: float
         """
-        self.cam.set_exposure(int(seconds*1e6))
-        print("Exposure time set to", 1000*seconds, "ms")
-    
+        self.cam.set_exposure(int(seconds * 1e6))
+        print("Exposure time set to", 1000 * seconds, "ms")
+
     def get_exposure_time(self):
         """This method gets the exposure time in seconds for the camera.
         """
-        return self.cam.get_exposure()/1e6
-    
+        return self.cam.get_exposure() / 1e6
+
     def set_auto_white_balance(self, toggle):
         """Enable/Disable auto white balance
-        
+
         :param toggle: True if auto white balance
         :type toggle: bool
         """
-        self.cam.set_param('auto_wb', toggle)
-    
+        self.cam.set_param("auto_wb", toggle)
+
     def set_trigger_mode(self, external):
         """Set external trigger
         """
@@ -90,7 +95,7 @@ class Ximea_Camera(Camera):
             self.cam.set_trigger_selector("XI_TRG_SEL_EXPOSURE_START")
         else:
             self.cam.set_trigger_source("XI_TRG_OFF")
-    
+
     def set_limit_bandwidth(self, limit):
         """Enable limit bandwidth (useful if there are several cameras
         acquiring simultaneously).
@@ -115,7 +120,7 @@ class Ximea_Camera(Camera):
         This function awaits for next image to be available in transport buffer.
         Color images are in BGR order (similar to OpenCV default).
         Attention: matplotlib expects RGB order.
-        
+
         :param image: Image instance to copy image to
         :type image: :class:`xiapi.Image`
         :param timeout: timeout in milliseconds
@@ -128,12 +133,12 @@ class Ximea_Camera(Camera):
             ctypes.wintypes.DWORD(timeout),
             ctypes.byref(image),
         )
-        
+
         if stat == 10:
             raise CameraTimeout
         elif stat != 0:
             raise xiapi.Xi_error(stat)
-     
+
     async def acquisition_async(
         self,
         num=np.inf,
@@ -143,18 +148,22 @@ class Ximea_Camera(Camera):
         raise_on_timeout=True,
     ):
         """Concrete implementation of :meth:`pymanip.video.Camera.acquisition_async` for the Ximea camera.
-        
+
         timeout in milliseconds.
         """
         loop = asyncio.get_event_loop()
         if timeout is None:
-            timeout = max((5000, 5*self.get_exposure_time()*1000))
+            timeout = max((5000, 5 * self.get_exposure_time() * 1000))
         img = xiapi.Image()
         self.cam.start_acquisition()
         try:
             count = 0
             while count < num:
-                if count == 0 and initialising_cams is not None and self in initialising_cams:
+                if (
+                    count == 0
+                    and initialising_cams is not None
+                    and self in initialising_cams
+                ):
                     initialising_cams.remove(self)
                 try:
                     await self.get_image(loop, img, timeout)
@@ -170,9 +179,9 @@ class Ximea_Camera(Camera):
                 stop_signal = yield MetadataArray(
                     img.get_image_data_numpy(),  # no copy
                     metadata={
-                            "counter": count,
-                            "timestamp": img.tsSec + img.tsUSec*1e-6,
-                        },
+                        "counter": count,
+                        "timestamp": img.tsSec + img.tsUSec * 1e-6,
+                    },
                 )
                 count += 1
                 if stop_signal:
