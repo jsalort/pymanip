e7dabb1157f670e8fb4001694106f62ac62b6cc7
Julien Salort <julien.salort@ens-lyon.fr>
2023-06-12 15:02:41 +0200

    Remove unused import

--- pymanip/aioinstruments/cryocon_24c.py
+++ pymanip/aioinstruments/cryocon_24c.py
@@ -8,7 +8,6 @@
 
 """
 
-import asyncio
 import fluidlab.instruments.multiplexer.cryocon_24c as fl_cryocon
 from pymanip.aioinstruments.aioiec60488 import AsyncIEC60488
 from pymanip.aioinstruments.aiosocket import AsyncSocketInstrument
