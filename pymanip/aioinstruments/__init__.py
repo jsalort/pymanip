d4981840dcd5d222334e9c4882740b02efca41c5
Julien Salort <mail@juliensalort.org>
2024-05-28 09:01:56 +0200

    Add Stanford DS360 in aioinstruments

--- pymanip/aioinstruments/__init__.py
+++ pymanip/aioinstruments/__init__.py
@@ -8,6 +8,7 @@ This module auto-imports all the asynchronous instrument classes.
 from .agilent_34970a import AsyncAgilent34970a  # noqa: F401
 from .agilent_6030a import AsyncAgilent6030a  # noqa: F401
 from .stanford_sr830 import AsyncStanfordSR830  # noqa: F401
+from .srs_ds360 import AsyncStanfordDS360  # noqa: F401
 from .newport_xps_rl import AsyncNewportXpsRL  # noqa: F401
 from .tdk_lambda import AsyncTdkLambda  # noqa: F401
 from .xantrex_xdc300 import AsyncXantrexXDC300  # noqa: F401
